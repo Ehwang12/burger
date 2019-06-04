509017672e63ba5f2e408290330c9c473c265a43
Esther <estshwang@gmail.com>
2019-06-04 01:29:28 -0500

    server and connection files should be set up and good to go

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,2 +1,8 @@
-const express = require("express");
-const app = express();
+var express = require("express");
+
+var router = express.Router();
+
+// Import the model (cat.js) to use its database functions.
+var burger = require("../models/burger.js");
+
+
--- server.js
+++ server.js
@@ -17,12 +17,10 @@ app.set("view engine", "handlebars");
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
-// Routes
-// ===========================================================
-app.get("/", function(req, res) {
-    res.render("index")
-  });
-  
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);  
   
 // Starts the server to begin listening
 // =============================================================
